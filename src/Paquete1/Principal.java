e463ff1720892305c67abe429e1f1c8765a5c147
mati <mati@daw1-07>
2025-03-06 11:28:59 +0100

    hola

---
+++ src/Paquete1/Principal.java
@@ -0,0 +1,21 @@
+package Paquete1;
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Estamos haciendo control de versiones de Git en desarollo de entorno");
+		System.out.println("Estamos haciendo control de versiones de Git en desarollo de entorno");
+		System.out.println("Estamos haciendo control de versiones de Git en desarollo de entorno");
+		System.out.println("Estamos haciendo control de versiones de Git en desarollo de entorno");
+		System.out.println("Estamos haciendo control de versiones de Git en desarollo de entorno");
+		System.out.println("Estamos haciendo control de versiones de Git en desarollo de entorno");
+		System.out.println("Estamos haciendo control de versiones de Git en desarollo de entorno");
+
+	}
+
+}
